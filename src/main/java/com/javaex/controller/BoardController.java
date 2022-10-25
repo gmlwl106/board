d5e447bb81f509136e813d90ddd0e3d33d98deb6
gmlwl106 <66778894@naver.com>
2022-10-25 09:26:26 +0900

    약간 수정

--- src/main/java/com/javaex/controller/BoardController.java
+++ src/main/java/com/javaex/controller/BoardController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -46,39 +47,46 @@ public class BoardController {
 	//게시글 폼
 	@RequestMapping(value="read/{no}", method= {RequestMethod.GET, RequestMethod.POST})
 	public String readForm(Model model, @PathVariable int no,
-			HttpServletRequest request, HttpServletResponse response,
+			HttpServletRequest request, HttpServletResponse response, HttpSession session,
 			@RequestParam(value="crtPage", required = false, defaultValue = "1") int crtPage,
 			@RequestParam(value="kwdOpt", required = false, defaultValue = "") String kwdOpt,
 			@RequestParam(value="keyword", required = false, defaultValue = "") String keyword) {
-		//쿠키로 조회수 중복 방지
-		Cookie oldCookie = null;
-		Cookie[] cookies = request.getCookies();
 		
-		if(cookies != null) {
-			for(Cookie cookie : cookies) {
-				if(cookie.getName().equals("postView")) {
-					oldCookie = cookie;
+		//로그인 안한 사용자가 접근했을때 조회수 안올라가게
+		if(session.getAttribute("authUser") != null) {
+			//쿠키로 조회수 중복 방지
+			Cookie oldCookie = null;
+			Cookie[] cookies = request.getCookies();
+			
+			if(cookies != null) {
+				for(Cookie cookie : cookies) {
+					if(cookie.getName().equals("postView")) {
+						oldCookie = cookie;
+					}
 				}
 			}
-		}
-		
-		if(oldCookie != null) {
-			if(!oldCookie.getValue().contains("[" + no + "]")) {
+			
+			if(oldCookie != null) {
+				if(!oldCookie.getValue().contains("[" + no + "]")) {
+					//해당 글을 조회하지 않았을때
+					//조회수 증가
+					boardService.viewHitUp(no);
+					oldCookie.setValue(oldCookie.getValue()+"[" + no + "]");
+					oldCookie.setPath("/");
+					oldCookie.setMaxAge(60);
+					response.addCookie(oldCookie);
+				}
+			} else {
+				//생성된 쿠키가 없을때
 				//조회수 증가
 				boardService.viewHitUp(no);
-				oldCookie.setValue(oldCookie.getValue()+"[" + no + "]");
-				oldCookie.setPath("/");
-				oldCookie.setMaxAge(60 * 60 * 24);
-				response.addCookie(oldCookie);
+				Cookie newCookie = new Cookie("postView","[" + no + "]");
+				newCookie.setPath("/");
+				newCookie.setMaxAge(60);
+				response.addCookie(newCookie);
 			}
-		} else {
-			//조회수 증가
-			boardService.viewHitUp(no);
-			Cookie newCookie = new Cookie("postView","[" + no + "]");
-			newCookie.setPath("/");
-			newCookie.setMaxAge(60 * 60 * 24);
-			response.addCookie(newCookie);
 		}
+		
 		
 		//게시글 가져오기
 		Map<String, Object> pMap = boardService.getPost(no);
