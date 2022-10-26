0d837b01c91d12bbe20daa0d33ce79929a57a598
gmlwl106 <66778894@naver.com>
2022-10-26 15:42:26 +0900

    스케쥴러

---
+++ src/main/java/com/javaex/service/SchedulerService.java
@@ -0,0 +1,13 @@
+package com.javaex.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulerService {
+	
+	@Scheduled(cron = "0/10 * * * * *")
+	public void doJob() {
+		System.out.println("스케줄링 테스트");
+	}
+}
